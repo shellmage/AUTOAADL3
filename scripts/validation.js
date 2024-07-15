092c3c3bbc64a23ffea5e2d95c36d223cc7e00e0
shellmage <kaddourafik@gmail.com>
2024-07-15 14:02:44 +0100

    Change border color if input is not valid

--- scripts/validation.js
+++ scripts/validation.js
@@ -11,10 +11,28 @@ document.addEventListener("DOMContentLoaded", () => {
     // Enable init form data button
     initButton.disabled = false;
 
+    wilayaSelect.style.border = "2px solid #a096a6eb";
+    ninInput.style.border = "2px solid #a096a6eb";
+    nssInput.style.border = "2px solid #a096a6eb";
+    phoneInput.style.border = "2px solid #a096a6eb";
+
     const wilayaValid = wilayaSelect.value !== "";
     const ninValid = /^\d{18}$/.test(ninInput.value);
     const nssValid = /^\d{12}$/.test(nssInput.value);
     const phoneValid = /^\d{10}$/.test(phoneInput.value);
+
+    if (!wilayaValid) {
+      wilayaSelect.style.border = "2px solid rgb(216 69 69)";
+    }
+    if (!ninValid) {
+      ninInput.style.border = "2px solid rgb(216 69 69)";
+    }
+    if (!nssValid) {
+      nssInput.style.border = "2px solid rgb(216 69 69)";
+    }
+    if (!phoneValid) {
+      phoneInput.style.border = "2px solid rgb(216 69 69)";
+    }
 
     submitButton.disabled = !(
       wilayaValid &&
